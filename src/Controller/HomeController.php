04d1bfc6bfc4f76236669aed5c60b5596303ea50
Destin99 <68994068+Celin89@users.noreply.github.com>
2023-06-02 19:53:06 +0200

    Btech-Learning_Fr v2

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,28 +2,17 @@
 
 namespace App\Controller;
 
-use App\Entity\Banner;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    private $entityManager;
-
-    public function __construct(EntityManagerInterface $entityManager)
-    {
-        $this->entityManager = $entityManager;
-    }
-
     #[Route('/', name: 'home')]
     public function index(): Response
     {
-        $banner = $this->entityManager->getRepository(Banner::class)->findAll();
-
         return $this->render('home/index.html.twig', [
-            'images' => $banner,
+            'controller_name' => 'HomeController',
         ]);
     }
 }
